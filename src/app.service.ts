61454e43bd992738cfd69c2d485b05203f1dfadd
manh.pham <manh.pham@savvycomsoftware.com>
2024-05-20 00:10:46 +0700

    fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -51,7 +51,7 @@ export class CameraService {
         '-c:v libx264',
         '-vf scale=1280:720',
         '-f segment',
-        '-segment_time 3000',
+        '-segment_time 3600',
         '-reset_timestamps 1',
         '-strftime 1',
       ])
