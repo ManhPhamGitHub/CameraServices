fb3d49c190fa3582298d60166d034967b691c7e9
manh.pd <manh.pd@cmctelecom.vn>
2024-05-22 17:51:32 +0700

    fix

--- src/constant.ts
+++ src/constant.ts
@@ -3,8 +3,8 @@ export const mockData = [
     cameraUrl: 'rtsp://rtsp-test-server.viomic.com:554/stream',
     name: 'Camera_1',
   },
-  {
-    cameraUrl: 'rtsp://rtsp-test-server.viomic.com:554/stream',
-    name: 'Camera_2',
-  },
+//   {
+//     cameraUrl: 'rtsp://rtsp-test-server.viomic.com:554/stream',
+//     name: 'Camera_2',
+//   },
 ];
