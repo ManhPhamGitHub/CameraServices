25f4d4842411fb2b88422e58203ab8376aa1ce89
manh.pd <manh.pd@cmctelecom.vn>
2024-06-05 14:22:22 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { CameraService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   controllers: [AppController],
   providers: [CameraService],
-  imports: [ScheduleModule.forRoot(), TasksModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    TasksModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'storage'),
+    }),
+  ],
 })
 export class AppModule {}
